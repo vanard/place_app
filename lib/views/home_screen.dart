b9fb48776da9f488e5f84dd9ce72ff7a199210ad
vanard <vianixa34@gmail.com>
2025-01-14 20:19:07 +0700

    update progress home ui

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:place_app/const.dart';
+import 'package:place_app/models/place_model.dart';
+import 'package:place_app/widgets/see_all.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -22,8 +24,119 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kBackgroundColor,
       appBar: appBar(),
-      body: Align(
+      body: Column(
+        children: [
+          const SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Popular Places",
+                  style: TextStyle(
+                    fontSize: 21,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(
+                      color: Colors.black12,
+                    ),
+                  ),
+                  child: SeeAll(),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 15),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: Row(
+              children: List.generate(
+                popular.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: GestureDetector(
+                    onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (_) => PlaceDetailScreen(
+                      //       destination: popular[index],
+                      //     ),
+                      //   ),
+                      // );
+                    },
+                    child: PopularPlaces(
+                      place: popular[index],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 15),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Recomendation For You",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(
+                      color: Colors.black12,
+                    ),
+                  ),
+                  child: SeeAll(),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                children: List.generate(
+                  popular.length,
+                  (index) => Padding(
+                    padding: const EdgeInsets.only(bottom: 15),
+                    child: GestureDetector(
+                      onTap: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (_) => PlaceDetailScreen(
+                        //       destination: recomendate[index],
+                        //     ),
+                        //   ),
+                        // );
+                      },
+                      child: RecomendList(place: popular[index]),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 15),
+          Align(
             alignment: Alignment.bottomCenter,
             child: Column(
               mainAxisSize: MainAxisSize.min,
@@ -42,7 +155,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           borderRadius: BorderRadius.circular(10),
                         ),
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: List.generate(
                             icons.length,
                             (index) => GestureDetector(
@@ -69,66 +182,329 @@ class _HomeScreenState extends State<HomeScreen> {
               ],
             ),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class RecomendList extends StatelessWidget {
+  final Place place;
+  const RecomendList({
+    super.key, required this.place
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 5,
+      ),
+      child: Stack(
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 95,
+                width: 110,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      place.photos[0],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      place.name,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        wordSpacing: 1,
+                        letterSpacing: 0.5,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.location_on,
+                          color: Colors.black,
+                          size: 16,
+                        ),
+                        Text(
+                          place.location,
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black
+                                .withTransparent(0.6),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 5),
+                    Row(
+                      children: [
+                        Text.rich(
+                          TextSpan(
+                            children: [
+                              TextSpan(
+                                text:
+                                    "${place.rate}",
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              TextSpan(
+                                text:
+                                    " (${place.review} reviews)",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black
+                                      .withTransparent(0.6),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Column(
+              children: [
+                const Spacer(),
+                Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "\$${place.price}",
+                        style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.blueGrey),
+                      ),
+                      TextSpan(
+                        text: " /Person",
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black.withTransparent(0.6),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class PopularPlaces extends StatelessWidget {
+  final Place place;
+
+  const PopularPlaces({
+    super.key,
+    required this.place,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          bottom: 0,
+          right: 20,
+          left: 20,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12.withTransparent(0.1),
+                  spreadRadius: 15,
+                  blurRadius: 5,
+                ),
+              ],
+            ),
+          ),
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Container(
+            height: 210,
+            width: MediaQuery.of(context).size.width * 0.75,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image:
+                    // NetworkImage(
+                    //   place.photos[0],
+                    // ),
+                    Image.network(place.photos[0],
+                        errorBuilder: (context, error, stackTrace) {
+                  return Container(
+                      height: 400, child: Text('No Images to Display'));
+                }).image,
+              ),
+            ),
+            child: Column(
+              children: [
+                const Spacer(),
+                Container(
+                  color: Colors.black.withTransparent(0.7),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              place.name,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                const Icon(
+                                  Icons.location_on,
+                                  color: Colors.white,
+                                  size: 18,
+                                ),
+                                const SizedBox(width: 5),
+                                Text(
+                                  place.location,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star_rounded,
+                              size: 22,
+                              color: Colors.amber[800],
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              place.rate.toString(),
+                              style: const TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
 
 AppBar appBar() {
-    return AppBar(
-      elevation: 0,
-      backgroundColor: Colors.white,
-      leadingWidth: 250,
-      leading: const Row(
-        children: [
-          SizedBox(width: 15),
-          Icon(
-            Icons.pin_drop_outlined,
+  return AppBar(
+    elevation: 0,
+    backgroundColor: Colors.white,
+    leadingWidth: 250,
+    leading: const Row(
+      children: [
+        SizedBox(width: 15),
+        Icon(
+          Icons.pin_drop_outlined,
+          color: Colors.black,
+        ),
+        SizedBox(width: 5),
+        Text(
+          "Bali, Indonesia",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 18,
             color: Colors.black,
           ),
-          SizedBox(width: 5),
-          Text(
-            "Bali, Indonesia",
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 18,
-              color: Colors.black,
-            ),
-          ),
-          Icon(
-            Icons.keyboard_arrow_down,
-            size: 30,
-            color: Colors.black26,
-          ),
-        ],
-      ),
-      actions: [
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            border: Border.all(
-              color: Colors.black12,
-            ),
-          ),
-          padding: const EdgeInsets.all(7),
-          child: const Stack(
-            children: [
-              Icon(
-                Icons.notifications_outlined,
-                color: Colors.black,
-                size: 30,
-              ),
-              Positioned(
-                top: 5,
-                right: 5,
-                child: CircleAvatar(
-                  radius: 5,
-                  backgroundColor: Colors.red,
-                ),
-              ),
-            ],
+        ),
+        Icon(
+          Icons.keyboard_arrow_down,
+          size: 30,
+          color: Colors.black26,
+        ),
+      ],
+    ),
+    actions: [
+      Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(
+            color: Colors.black12,
           ),
         ),
-        const SizedBox(width: 15),
-      ],
-    );
-  }
+        padding: const EdgeInsets.all(6),
+        child: const Stack(
+          children: [
+            Icon(
+              Icons.notifications_outlined,
+              color: Colors.black,
+              size: 30,
+            ),
+            Positioned(
+              top: 5,
+              right: 5,
+              child: CircleAvatar(
+                radius: 5,
+                backgroundColor: Colors.red,
+              ),
+            ),
+          ],
+        ),
+      ),
+      const SizedBox(width: 15),
+    ],
+  );
+}
