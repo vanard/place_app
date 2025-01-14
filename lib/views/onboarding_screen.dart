8dbe664ed42cde72d2c8808e21b3a171a4a1d642
vanard <vianixa34@gmail.com>
2025-01-14 14:27:39 +0700

    create onboarding screen

---
+++ lib/views/onboarding_screen.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:place_app/const.dart';
+import 'package:place_app/models/onboarding_model.dart';
+import 'package:place_app/views/home_screen.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  int currentIndex = 0;
+  Widget dotIndicator(int index) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 400),
+      margin: const EdgeInsets.only(right: 6),
+      width: 30,
+      height: 5,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: index == currentIndex ? Colors.white : Colors.white54,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var widthApp = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView.builder(
+            scrollBehavior: AppScrollBehavior(),
+            itemCount: onboarding.length,
+            onPageChanged: (value) {
+              setState(() {
+                currentIndex = value;
+              });
+            },
+            itemBuilder: (context, index) {
+              return Image.network(
+                onboarding[index].image,
+                fit: BoxFit.cover,
+              );
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const SizedBox(height: 40),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const HomeScreen(),
+                            ),
+                            (route) => false);
+                      },
+                      child: Visibility(
+                        visible: onboarding.length - 1 != currentIndex && widthApp < 1000
+                            ? true
+                            : false,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 7,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.white54,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Text(
+                            "Skip",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          onboarding[currentIndex].name,
+                          style: const TextStyle(
+                            fontSize: 70,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            height: 1,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          onboarding[currentIndex].description,
+                          style: const TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              height: 180,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      onboarding.length,
+                      dotIndicator,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(35),
+                    child: Container(
+                      padding: const EdgeInsets.all(35),
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (_) => const HomeScreen(),
+                                  ),
+                                  (route) => false);
+                            },
+                            child: Container(
+                              height: 75,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.black12,
+                                    offset: Offset(0, 5),
+                                    spreadRadius: 15,
+                                    blurRadius: 15,
+                                  ),
+                                ],
+                                color: kButtonColor,
+                                // color: Color(0xff1b1b1b),
+                              ),
+                              child: Center(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Let's Get Started",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(width: 5),
+                                    Icon(
+                                      Icons.arrow_forward,
+                                      color: Colors.white,
+                                      size: 25,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AppScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.trackpad,
+      };
+}
