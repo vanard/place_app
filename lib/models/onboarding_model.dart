cd959a47412b7da55850a3ed5ee2ba26bd8b21a5
vanard <vianixa34@gmail.com>
2025-01-14 20:18:17 +0700

    update resolution image

--- lib/models/onboarding_model.dart
+++ lib/models/onboarding_model.dart
@@ -6,22 +6,22 @@ class OnboardModel {
 
 List<OnboardModel> onboarding = [
    OnboardModel(
-    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/33/fc/fd/denpasar.jpg?w=600&h=500',
+    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/33/fc/fd/denpasar.jpg?w=1000&h=500',
     name: 'Explore Bali with us.',
     description: description,
   ),
   OnboardModel(
-    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/03/9b/2f/be/kuta.jpg?w=600&h=500',
+    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/03/9b/2f/be/kuta.jpg?w=1000&h=500',
     name: "Natural Beauty of Bali",
     description: description,
   ),
   OnboardModel(
-    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/59/d9/1c/caption.jpg?w=600&h=500',
+    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/59/d9/1c/caption.jpg?w=1000&h=500',
     name: 'Peacefull Mind in Nature',
     description: description,
   ),
   OnboardModel(
-    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/ae/df/98/prana-spa.jpg?w=600&h=500',
+    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/ae/df/98/prana-spa.jpg?w=1000&h=500',
     name: 'Cultural Diversity of Bali',
     description: description,
   ),
