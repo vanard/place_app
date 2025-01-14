0a9153b855d06f8161b77d07441016cc2e7e0873
vanard <vianixa34@gmail.com>
2025-01-14 14:33:05 +0700

    create home screen

---
+++ lib/views/home_screen.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:place_app/const.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedPage = 0;
+
+  List<IconData> icons = [
+    Icons.home,
+    Icons.search,
+    Icons.shopping_cart,
+    Icons.bookmark_outline,
+    Icons.person_outline,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(),
+      body: Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 22,
+                        ),
+                        decoration: BoxDecoration(
+                          color: kButtonColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: List.generate(
+                            icons.length,
+                            (index) => GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  selectedPage = index;
+                                });
+                              },
+                              child: Icon(
+                                icons[index],
+                                size: 32,
+                                color: selectedPage == index
+                                    ? Colors.white
+                                    : Colors.white.withTransparent(0.4),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+    );
+  }
+}
+
+AppBar appBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Colors.white,
+      leadingWidth: 250,
+      leading: const Row(
+        children: [
+          SizedBox(width: 15),
+          Icon(
+            Icons.pin_drop_outlined,
+            color: Colors.black,
+          ),
+          SizedBox(width: 5),
+          Text(
+            "Bali, Indonesia",
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 18,
+              color: Colors.black,
+            ),
+          ),
+          Icon(
+            Icons.keyboard_arrow_down,
+            size: 30,
+            color: Colors.black26,
+          ),
+        ],
+      ),
+      actions: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(
+              color: Colors.black12,
+            ),
+          ),
+          padding: const EdgeInsets.all(7),
+          child: const Stack(
+            children: [
+              Icon(
+                Icons.notifications_outlined,
+                color: Colors.black,
+                size: 30,
+              ),
+              Positioned(
+                top: 5,
+                right: 5,
+                child: CircleAvatar(
+                  radius: 5,
+                  backgroundColor: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(width: 15),
+      ],
+    );
+  }
