dc08d78aedffe7080f722ca04e3d5cdeb305a0fe
vanard <vianixa34@gmail.com>
2025-01-14 14:26:52 +0700

    create onboarding model

---
+++ lib/models/onboarding_model.dart
@@ -0,0 +1,30 @@
+class OnboardModel {
+  String image, name, description;
+
+  OnboardModel({required this.image, required this.name, required this.description});
+}
+
+List<OnboardModel> onboarding = [
+   OnboardModel(
+    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/33/fc/fd/denpasar.jpg?w=600&h=500',
+    name: 'Explore Bali with us.',
+    description: description,
+  ),
+  OnboardModel(
+    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/03/9b/2f/be/kuta.jpg?w=600&h=500',
+    name: "Natural Beauty of Bali",
+    description: description,
+  ),
+  OnboardModel(
+    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/59/d9/1c/caption.jpg?w=600&h=500',
+    name: 'Peacefull Mind in Nature',
+    description: description,
+  ),
+  OnboardModel(
+    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/ae/df/98/prana-spa.jpg?w=600&h=500',
+    name: 'Cultural Diversity of Bali',
+    description: description,
+  ),
+];
+
+const description = "We Travelin are ready to help you on\nvacation around Bali";
