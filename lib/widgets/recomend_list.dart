145e192d0658a0ddb2c8c06964f91b786a13f009
vanard <vianixa34@gmail.com>
2025-01-15 22:03:16 +0700

    add recomend places widget

---
+++ lib/widgets/recomend_list.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:place_app/ext.dart';
+import 'package:place_app/models/place_model.dart';
+
+class RecomendList extends StatelessWidget {
+  final Place place;
+  const RecomendList({
+    super.key, required this.place
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 5,
+      ),
+      child: Stack(
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 95,
+                width: 110,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      place.photos[0],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      place.name,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        wordSpacing: 1,
+                        letterSpacing: 0.5,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.location_on,
+                          color: Colors.black,
+                          size: 16,
+                        ),
+                        Text(
+                          place.location,
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black
+                                .withTransparent(0.6),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 5),
+                    Row(
+                      children: [
+                        Text.rich(
+                          TextSpan(
+                            children: [
+                              TextSpan(
+                                text:
+                                    "${place.rate}",
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              TextSpan(
+                                text:
+                                    " (${place.review} reviews)",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black
+                                      .withTransparent(0.6),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Column(
+              children: [
+                const Spacer(),
+                Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "\$${place.price}",
+                        style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.orange),
+                      ),
+                      TextSpan(
+                        text: " /Person",
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.black.withTransparent(0.6),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
