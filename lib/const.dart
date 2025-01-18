1716e905ae32cce5dbe86133174f38e7143f21e7
vanard <vianixa34@gmail.com>
2025-01-18 13:22:57 +0700

    add const for profile data

--- lib/const.dart
+++ lib/const.dart
@@ -2,3 +2,9 @@ import 'package:flutter/material.dart';
 
 const kButtonColor = Color(0xff1b1b1b);
 const kBackgroundColor = Color(0xfff2f3f4);
+
+//profile
+const kName = 'Vian Rasyid D';
+const kJob = 'Mobile Developer';
+const kCaption = '#OpenToWork';
+const kProfileImage = 'https://avatars.githubusercontent.com/u/25784574?v=4';
